673049e6fabb20c1db1d4d47be6581841ac15681
ThiagoJoelP <thiagojoel17@hotmail.com>
2026-05-13 01:15:09 -0300

    feat: pagina Materiales funcional completa

--- src/pages/Materiales.jsx
+++ src/pages/Materiales.jsx
@@ -1,3 +1,18 @@
+import SeccionPlacas from '@/components/materiales/SeccionPlacas'
+import SeccionCanto from '@/components/materiales/SeccionCanto'
+import SeccionHerrajes from '@/components/materiales/SeccionHerrajes'
+import styles from './Materiales.module.css'
+
 export default function Materiales() {
-  return <div><h1>Materiales</h1></div>
+  return (
+    <div className={styles.page}>
+      <div className={styles.pageHeader}>
+        <h1>Materiales y Precios</h1>
+        <p className={styles.hint}>Hace clic en cualquier celda para editar el valor.</p>
+      </div>
+      <SeccionPlacas />
+      <SeccionCanto />
+      <SeccionHerrajes />
+    </div>
+  )
 }
